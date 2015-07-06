a0790f5bd3fbca100463545145b9cd4402aef482
Ngữ Quang Trương (Nicholas) <zquangu112z@gmail.com>
2015-07-06 10:16:05 +0700

    user fragment

--- app/src/main/java/com/example/ngu/myinstagram/fragment/UserFragment.java
+++ app/src/main/java/com/example/ngu/myinstagram/fragment/UserFragment.java
@@ -1,23 +1,24 @@
 package com.example.ngu.myinstagram.fragment;
 
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.ngu.myinstagram.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link UserFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link UserFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+
 public class UserFragment extends Fragment {
+    ImageButton bt_three_point;
+    View rootView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +27,21 @@ public class UserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user, container, false);
+        rootView=inflater.inflate(R.layout.fragment_user, container, false);
+        return rootView;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        bt_three_point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager =getFragmentManager();
+                FragmentTransaction transaction=fragmentManager.beginTransaction();
+                transaction.replace(R.id.fg_fragment_user,new UserFragmentFlow());
+                transaction.commit();
+            }
+        });
+    }
 }
